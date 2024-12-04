ce3ec85e82111bc94e2251be56d79279a4698320
JoseMarcolino1 <josetyu52@gmail.com>
2024-12-04 15:44:50 -0300

    refactor: mudando o card component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
